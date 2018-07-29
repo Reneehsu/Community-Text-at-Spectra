3df5115fbf5e4bcaadf66461b2dc1f5b90ad18c7
git push -u origin <renee8688@gmail.com>
2018-07-28 19:21:32 -0700

    something

--- schedule.js
+++ schedule.js
@@ -1,4 +1,5 @@
 var client = require('twilio')(process.env.TWILIO_SID, process.env.TWILIO_AUTH_TOKEN);
+console.log('schedule');
 client.messages.create({
   to: '+13109233881',
   from: '+14245238634',
